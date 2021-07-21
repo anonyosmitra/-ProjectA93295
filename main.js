7b8ce2f5cad858fc34513b85966880354ea35f93
andro <anonyos.mitra@gmail.com>
2021-07-21 12:46:55 +0530

    A

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const https = require('https');
-const fs = require('fs');
-const options = {
-  key: fs.readFileSync('keys/privkey.pem'),
-  cert: fs.readFileSync('keys/fullchain.pem')
+const crypto = require('crypto'),
+      fs = require("fs"),
+      http = require("http");
+
+var privateKey = fs.readFileSync('keys/privkey.pem').toString();
+var certificate = fs.readFileSync('keys/fullchain.pem').toString();
+
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+
+var handler = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 };
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(5015);
+var server = http.createServer();
+server.setSecure(credentials);
+server.addListener("request", handler);
+server.listen(5015)
